644bbea2f89b5fbdee9ec58e6c744e36c8eb2432
Wesley Wigham <wwigham@gmail.com>
2017-04-06 14:52:28 -0400

    We super fancy now

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import * as React from "react";
 import "./App.css";
-import * as solved from "solved";
 import { Grid, GridElement } from "./components";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import { Slider } from "material-ui";
 
-const logo = require("./logo.svg");
-
 interface AppState {
   size: number;
   values: GridElement[][];
+  symmetry: boolean;
+  title: string;
+}
+
+interface WordRef {
+  pos: {i: number, j: number};
+  pattern: string;
+  reference: number;
+  text: string;
 }
 
 function newGridValues(width: number, height: number) {
@@ -40,29 +46,34 @@ function clone(state: AppState) {
 }
 
 class App extends React.Component<null, AppState> {
+  private across: WordRef[] = [];
+  private down: WordRef[] = [];
   constructor() {
     super();
     this.state = {
       size: 15,
-      values: newGridValues(15, 15)
+      symmetry: true,
+      values: newGridValues(15, 15),
+      title: "",
     };
   }
   render() {
+    const reflect = (i: number) => ((i + 1 - this.state.size) * -1);
+    this.across = this.generateAcross();
+    this.down = this.generateDown(this.across);
+
     return (
       <MuiThemeProvider>
         <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
-          </div>
-          <p className="App-intro">
-            <small>To get started, edit <code>src/App.tsx</code> and save to reload.</small>
-            <br/>
-            <small>Available slitherlink strategies: {Object.keys(solved.Slitherlink.Strategies)
-              .filter(k => k !== "all" && k !== "register").join(", ")}</small>
-          </p>
           <div className="grid-container">
-            <h3>Grid Size</h3>
+            <h1>Title:
+              <span
+                contentEditable={true}
+                onInput={(e) => this.setState({...this.state, title: (e.target as HTMLSpanElement).innerText})}
+              >{this.state.title}
+              </span>
+            </h1>
+            <h3>Grid Size: {this.state.size}</h3>
             <div className="top-slider">
               <Slider
                 max={21}
@@ -86,15 +97,159 @@ class App extends React.Component<null, AppState> {
               onContextMenu={(event, cell, i, j) => {
                 const nextState = clone(this.state);
                 nextState.values[i][j].enabled = !nextState.values[i][j].enabled;
+                if (nextState.values[i][j].enabled === false) {
+                  nextState.values[i][j].value = "";
+                }
+                if (this.state.symmetry) {
+                  nextState.values[reflect(i)][reflect(j)].enabled
+                     = nextState.values[i][j].enabled;
+                  if (!nextState.values[reflect(i)][reflect(j)].enabled) {
+                    nextState.values[reflect(i)][reflect(j)].value = "";
+                  }
+                }
                 this.setState(nextState);
                 event.preventDefault();
                 event.stopPropagation();
               }}
             />
+            <div>
+              <label>Enforce Symmetry</label>
+              <input
+                type="checkbox"
+                checked={this.state.symmetry}
+                onChange={() => this.setState({...this.state, symmetry: !this.state.symmetry})}
+              />
+              <button onClick={() => this.fill()}>Fill</button>
+              <button onClick={() => this.export()}>Export</button>
+            </div>
+            <div className="across-list">
+              <h1>Across</h1>
+              <ul className="unstyled-list">
+              {
+                this.across.map((e, i) => (
+                  <li>
+                    <h5>{e.reference}. {e.pattern} @ {e.pos.i},{e.pos.j}</h5>
+                    <textarea onChange={(evt) => this.across[i].text = evt.target.value}/>
+                  </li>
+                ))
+              }
+              </ul>
+            </div>
+            <div className="down-list">
+              <h1>Down</h1>
+              <ul className="unstyled-list">
+              {
+                this.down.map((e, i) => (
+                  <li>
+                    <h5>{e.reference}. {e.pattern} @ {e.pos.i},{e.pos.j}</h5>
+                    <textarea onChange={(evt) => this.down[i].text = evt.target.value}/>
+                  </li>
+                ))
+              }
+              </ul>
+            </div>
           </div>
         </div>
       </MuiThemeProvider>
     );
+  }
+  private fill() {
+    void 0;
+  }
+  private export() {
+    const tab = "\t";
+    const text = `<ACROSS PUZZLE>
+<TITLE>
+${tab}${this.state.title || "Untitled"}
+<COPYRIGHT>
+${tab}Author
+<SIZE>
+${tab}${this.state.size}x${this.state.size}
+<GRID>
+${this.state.values.map(r => `${tab}${r.map(e => e.enabled ? (e.value || "?") : ".").join("")}`).join("\n")}
+<ACROSS>
+${this.across.map(a => `${tab}${a.text || "No hint!"}`).join("\n")}
+<DOWN>
+${this.down.map(d => `${tab}${d.text || "No hint!"}`).join("\n")}`;
+    const filename = `${this.state.title}.puz`;
+    const element = document.createElement("a");
+    element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(text));
+    element.setAttribute("download", filename);
+
+    element.style.display = "none";
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  }
+  private generateAcross() {
+    const self = this;
+    const words: WordRef[] = [];
+    let buf = "";
+    let inWordState = false;
+    let bufStart: {i: number, j: number} = {i: 0, j: 0};
+    for (let i = 0; i < this.state.values.length; i++) {
+      for (let j = 0; j < this.state.values[i].length; j++) {
+        const val = this.state.values[i][j];
+        if (!inWordState && val.enabled) {
+          inWordState = true;
+          buf += val.value ? val.value : "?";
+          bufStart = {i, j};
+        } else if (inWordState && val.enabled) {
+          buf += val.value ? val.value : "?";
+        } else if (inWordState && !val.enabled) {
+          finishWord();
+        }
+      }
+      finishWord();
+    }
+    return words;
+
+    function finishWord() {
+      inWordState = false;
+      if (buf.length === 0) { return; } 
+      words.push({
+        pos: bufStart,
+        pattern: buf,
+        reference: words.length + 1,
+        text: (self.across[words.length + 1] || {text: ""}).text
+      });
+      buf = "";
+    }
+  }
+  private generateDown(across: WordRef[]): WordRef[] {
+    const words: WordRef[] = [];
+    let prevIndex = across.length + 1;
+    let buf = "";
+    let inWordState = false;
+    let bufStart: {i: number, j: number} = {i: 0, j: 0};
+    for (let j = 0; j < this.state.values[0].length; j++) {
+      for (let i = 0; i < this.state.values.length; i++) {
+        const val = this.state.values[i][j];
+        if (!inWordState && val.enabled) {
+          inWordState = true;
+          buf += val.value ? val.value : "?";
+          bufStart = {i, j};
+        } else if (inWordState && val.enabled) {
+          buf += val.value ? val.value : "?";
+        } else if (inWordState && !val.enabled) {
+          finishWord();
+        }
+      }
+      finishWord();
+    }
+    words.sort((a, b) => a.reference - b.reference);
+    for (let i = 0; i < words.length; i++) {
+        words[i].text = (this.down[i] || {text: ""}).text;
+    }
+    return words;
+
+    function finishWord() {
+      inWordState = false;
+      if (buf.length === 0) { return; }
+      const matching = across.find(w => w.pos.i === bufStart.i && w.pos.j === bufStart.j);
+      words.push({pos: bufStart, pattern: buf, reference: matching ? matching.reference : prevIndex++, text: ""});
+      buf = "";
+    }
   }
 }
 
